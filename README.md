e1f9d988b2b6d92ee1d0dfb7a942f2556e8b6509
Omar Flores <cerberusprogrammer@gmail.com>
2024-06-08 15:44:53 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# clean_notes
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/clean_notes/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.clean_notes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
