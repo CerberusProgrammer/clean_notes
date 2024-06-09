8e2eebd58521733e9a7f5ac1521839cc48646759
Omar Flores <cerberusprogrammer@gmail.com>
2024-06-08 18:34:01 -0700

    feat: :sparkles: New themedata consumer

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,18 @@
+import 'package:clean_notes/theme.provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+void main() => runApp(const ProviderScope(child: MainApp()));
 
-class MainApp extends StatelessWidget {
+class MainApp extends ConsumerWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
+  Widget build(BuildContext context, WidgetRef ref) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ref.watch(themeProvider),
+      home: const Scaffold(
         body: Center(
           child: Text('Hello World!'),
         ),
